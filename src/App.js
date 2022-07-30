5e96aebe4da0c65d1add36c53c2845cf1c5dd60d
aruadecarvalho <aruadecarvalho@gmail.com>
2022-07-30 08:54:29 -0300

    category-item, directory and routes first implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./routes/home/home.component";
+import { Routes, Route, Outlet } from "react-router-dom";
 
-function App() {
+const Navigation = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>I'm the nav bar</h1>
+      </div>
+      <Outlet />
     </div>
   );
-}
+};
+
+const Shop = () => {
+  return <h2>LOJA DO MOZI</h2>;
+};
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,26 @@
+import Directory from "../../components/directory/directory.component";
+
+const Home = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+      imageUrl:
+        "https://i.ibb.co/b5Lmpfj/DALL-E-2022-07-29-19-27-03-picture-of-a-cap-with-a-solid-light-color-background.png",
+    },
+    {
+      id: 2,
+      title: "Clothing",
+      imageUrl:
+        "https://i.ibb.co/025mWhr/DALL-E-2022-07-29-19-20-06-image-of-a-t-shirt-with-a-solid-light-color-background.png",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      imageUrl: "https://i.ibb.co/pfH9gRF/shoes.webp",
+    },
+  ];
+  return <Directory categories={categories} />;
+};
+
+export default Home;
