431bfc042ba319bfd9c64cadb4cd99f388615bad
aruadecarvalho <aruadecarvalho@gmail.com>
2022-07-30 16:36:40 -0300

    added firebase utils, firebase libraries

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,16 @@
+import {
+  signInWithGooglePopUp,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopUp();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
   return (
     <div>
       <h1>Sign In</h1>
+      <button onClick={logGoogleUser}>Sign in with google pop up</button>
     </div>
   );
 };
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,55 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAxefVWZoi1bBRq-j_5bftfyTHM78q9C3k",
+  authDomain: "ecommerce-site-c5dd8.firebaseapp.com",
+  projectId: "ecommerce-site-c5dd8",
+  storageBucket: "ecommerce-site-c5dd8.appspot.com",
+  messagingSenderId: "11634967269",
+  appId: "1:11634967269:web:89cb71b6b5462398c58a0c",
+};
+
+// Initialize Firebase
+const fireBaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopUp = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+  return userDocRef;
+};
