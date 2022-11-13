aeea5f9ecfcabc29770ca14a73bba59aeb495288
aruadecarvalho <aruadecarvalho@gmail.com>
2022-11-13 11:30:28 -0300

    added redux-persist

--- src/index.js
+++ src/index.js
@@ -5,16 +5,18 @@ import "swiper/css/bundle";
 import "./index.scss";
 import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
 import reportWebVitals from "./reportWebVitals";
-
+import { PersistGate } from "redux-persist/integration/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
