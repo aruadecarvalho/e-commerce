09dced6427ddf253568b654f14cfb59c1a0869eb
aruadecarvalho <aruadecarvalho@gmail.com>
2022-11-18 18:44:10 -0300

    userSaga, checkUserSession saga

--- src/App.js
+++ src/App.js
@@ -3,24 +3,15 @@ import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import {
-  onAuthStateChangedListener,
-  createUserDocumentFromAuth,
-} from "./utils/firebase/firebase.utils";
-import { setCurrentUser } from "./store/user/user.action";
 import { Routes, Route } from "react-router-dom";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
+import { checkUserSession } from "./store/user/user.action";
 const App = () => {
   const dispatch = useDispatch();
+
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      dispatch(setCurrentUser(user));
-    });
-    return unsubscribe;
+    dispatch(checkUserSession());
   }, []);
 
   return (
