af8689d37a1d055726fdb5eff9547d7fc2879d0c
aruadecarvalho <aruadecarvalho@gmail.com>
2023-01-04 12:18:00 -0300

    added typescript to button component

---
+++ src/components/button/button.component.tsx
@@ -0,0 +1,34 @@
+import { FC, ButtonHTMLAttributes } from "react";
+
+import {
+  BaseButton,
+  GoogleSignInButton,
+  InvertedButton,
+} from "./button.styles";
+
+export enum BUTTON_TYPE_CLASSES {
+  base = "base",
+  google = "google-sign-in",
+  inverted = "inverted",
+}
+
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base): typeof BaseButton =>
+  ({
+    [BUTTON_TYPE_CLASSES.base]: BaseButton,
+    [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
+    [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
+  }[buttonType]);
+
+export type ButtonProps = {
+  buttonType?: BUTTON_TYPE_CLASSES;
+  isLoading?: boolean;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+// add is loading prop disable={isLoading}
+
+const Button: FC<ButtonProps> = ({ children, buttonType, ...otherProps }) => {
+  const CustomButton = getButton(buttonType);
+  return <CustomButton {...otherProps}>{children}</CustomButton>;
+};
+
+export default Button;
